150c5a862dd74fd622f94af545d92619a8ea3060
“Ryan” <pagsmanagement@gmail.com>
2022-05-28 19:22:07 -0500

    sounds

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-window.addEventListener('keydown', (e) => {
-    const audio = document.querySelector(`audio[data-key="${e.keycode}"]`)
+window.addEventListener('keydown', function (e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    console.log(audio)
     if (!audio) return
     audio.play()
 }) 
