922e0e15af84b00d753f58e28600607febd9adca
“Ryan” <pagsmanagement@gmail.com>
2022-05-28 20:28:07 -0500

    playing with transitions

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
+
+const title = document.getElementById('title')
+
 window.addEventListener('keydown', function (e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
-    console.log(audio)
+    const key = document.querySelector(`#key[data-key="${e.keyCode}"]`)
+
     if (!audio) return
+    audio.currentTime = 0
     audio.play()
-}) 
+
+    key.classList.add('.playing')
+})
+
+key.addEventListener('click', () => {
+    key.classList.add('playing')
+    title.innerText = "Playe"
+
+})
